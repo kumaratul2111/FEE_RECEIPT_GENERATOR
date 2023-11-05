24355e3d16131e72c1cb228fc068f456387e02ed
kumaratul2111 <kumaratul2111@gmail.com>
2023-11-05 17:24:16 +0530

    Updated the main logic

---
+++ main.py
@@ -0,0 +1,16 @@
+import GUI.generate_fee_receipt
+import GUI.record_payment
+import GUI.fee_receipt_generator as g
+import update_database
+
+g.val = 1
+while (g.val != 4) :
+    g.input_choice()
+    if g.val == 1 :
+        l = GUI.generate_fee_receipt.fee_receipt_generator()
+        print(l)
+    elif g.val == 2 :
+        l = GUI.record_payment.payment_recorder()
+        print(l)
+    elif g.val == 3 :
+        print("Nothing to do right now")
