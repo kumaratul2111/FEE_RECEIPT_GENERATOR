de0fead05c0b7b578773f0b7a42195b26b347d12
kumaratul2111 <kumaratul2111@gmail.com>
2023-11-26 14:14:16 +0530

    Updated database logic

--- update_database.py
+++ update_database.py
@@ -162,15 +162,61 @@ def record_payment_table(table_name, payment, month, year, roll) :
         if connection_object is not None :
             connection_object.close();
 
+def fetch_details(table_name, month, year) :
+    sql_fetch_details_query = "Select name, email, roll, class, " + month+"_TUITION_FEE_"+year + ", " + month+"_HOSTEL_FEE_"+year + ", " + month+"_TRANSPORTATION_FEE_"+year+ ", " + month+"_MESS_FEE_"+year+ ", " + month+"_OTHER_FEE_"+year+ ", " + "remaining_dues from " + table_name + ";"
+    print(sql_fetch_details_query)
+    connection_object = None
+    try:
+        connection_object = psycopg.connect("dbname=SCHOOL")
+        cursor_object = connection_object.cursor()
+        cursor_object.execute(sql_fetch_details_query)
+        rows = cursor_object.fetchall()
+        print("The number of rows = ", cursor_object.rowcount)
+        cursor_object.close()
+        return rows
+    except (Exception, psycopg.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection_object is not None:
+            connection_object.close()
+
+def fetch_details_of_a_student(table_name, month, year, roll) :
+    sql_fetch_student_details = "Select name, email, roll, class, " + month+"_TUITION_FEE_"+year + ", " + month+"_HOSTEL_FEE_"+year + ", " + month+"_TRANSPORTATION_FEE_"+year+ ", " + month+"_MESS_FEE_"+year+ ", " + month+"_OTHER_FEE_"+year+ ", " + "remaining_dues from " + table_name + " where roll = '" + roll + "';"
+    print(sql_fetch_student_details)
+    connection_object = None
+    try:
+        connection_object = psycopg.connect("dbname=SCHOOL")
+        cursor_object = connection_object.cursor()
+        cursor_object.execute(sql_fetch_student_details)
+        rows = cursor_object.fetchall()
+        print("The number of rows = ", cursor_object.rowcount)
+        cursor_object.close()
+        return rows
+    except (Exception, psycopg.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection_object is not None:
+            connection_object.close()
+
+def fetch_details_for_a_class(table_name, month, year) :
+    sql_fetch_class_details = "Select name , " + month+"_PAYMENT_"+year +  ", remaining_dues from " + table_name + ";"
+    print(sql_fetch_class_details)
+    connection_object = None
+    try:
+        connection_object = psycopg.connect("dbname=SCHOOL")
+        cursor_object = connection_object.cursor()
+        cursor_object.execute(sql_fetch_class_details)
+        rows = cursor_object.fetchall()
+        print("The number of rows = ", cursor_object.rowcount)
+        cursor_object.close()
+        return rows
+    except (Exception, psycopg.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection_object is not None:
+            connection_object.close()
 
 
-# update_tuition_fee("CLASS1", 11.3, "MR", "23")
-# update_hostel_fee("CLASS1", 12.4, "MR", "23")
-# update_mess_fee("CLASS1", 13.5, "MR", '23')
-# update_transportation_fee("CLASS1", 14.6, "MR", '23')
-# update_other_fee("CLASS1", 15.7, "MR", '23')
-# update_remaining_dues("CLASS1", "MR", '23')
 
-# record_payment_table("CLASS1", 20.1, "MR", "23", "1")
+#reset_table("CLASS7", "may", "2020")
 
-#reset_table("CLASS1", "MR", "23")
